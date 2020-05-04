4173595f15b29d0ce6e32f92e9d3aa7d5e53635e
Sushila <creationinbrain@gmail.com>
2020-05-03 20:06:25 -0700

    nested loop

---
+++ nestedloop.java
@@ -0,0 +1,12 @@
+class Loop {
+public static void main(String args[]){
+int k =1;
+for(int i =1 ; i<= 5; i++){
+ for(int j=k; j<=5; j++){ 
+ System.out.print(j);
+  } 
+  k = k + 1;
+ System.out.println();
+   }
+ }
+}
